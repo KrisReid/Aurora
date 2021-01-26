5742bd2c51ba4f9be56e0fb26ded79aa3e73df65
Kris Reid <krisreid_06@hotmail.com>
2021-01-26 18:56:06 +0000

    First

---
+++ Shared/FirstPage.swift
@@ -0,0 +1,20 @@
+//
+//  FirstPage.swift
+//  Aurora
+//
+//  Created by Kris Reid on 26/01/2021.
+//
+
+import SwiftUI
+
+struct FirstPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstPage_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstPage()
+    }
+}
---
+++ Shared/SecondPage.swift
@@ -0,0 +1,20 @@
+//
+//  SecondPage.swift
+//  Aurora
+//
+//  Created by Kris Reid on 26/01/2021.
+//
+
+import SwiftUI
+
+struct SecondPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondPage()
+    }
+}
