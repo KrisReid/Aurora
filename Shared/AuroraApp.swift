4f8cfad5f9b254131f84f635cb822ff9ee5d5bb9
Kris Reid <krisreid_06@hotmail.com>
2021-01-26 10:56:12 +0000

    Initial Commit

---
+++ Shared/AuroraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuroraApp.swift
+//  Shared
+//
+//  Created by Kris Reid on 26/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AuroraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
